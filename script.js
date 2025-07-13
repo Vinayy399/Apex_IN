9fc74579c8ac71f5cb15f64f92768a565e86a2bc
Vinay  Kumar Reddy <161055491+Vinayy399@users.noreply.github.com>
2025-07-13 12:05:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const message = "Thanks for visiting the Apex Web Page!";
+    console.log(message);
+    alert(message);
+});
